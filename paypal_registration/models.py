cdb81ed7c01d7c872ae997b51cb746c7b65adb1b
Dusty Phillips <dusty@archlinux.ca>
2010-06-16 16:41:52 -0600

    Ensure that accounts can only be activated if the registration is paid.

--- paypal_registration/models.py
+++ paypal_registration/models.py
@@ -5,3 +5,11 @@ from registration.models import RegistrationProfile, RegistrationManager
 class PaypalRegistrationProfile(RegistrationProfile):
     paid = models.BooleanField(default=False)
     objects = RegistrationManager()
+
+    def activate_user(self, activation_key):
+        try:
+            profile = self.get(activation_key=activation_key, paid=True)
+        except self.model.DoesNotExist:
+            return False
+
+        return super(PaypalRegistrationProfile, self).activate_user(activation_key)

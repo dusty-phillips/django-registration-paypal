0938b0ceca0695eb869038845ee96684f05d702b
Dusty Phillips <dusty@archlinux.ca>
2010-06-17 11:27:13 -0600

    release version with paid signal.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
         name="django-registration-paypal",
-        version="0.1.0",
+        version="0.1.2",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
         packages=['paypal_registration'],
-        url="",
+        url="http://github.com/buchuki/django-registration-paypal",
         license="LICENSE.txt",
         description="django-registration backend to support paypal payments",
         long_description=open('README.txt').read(),
